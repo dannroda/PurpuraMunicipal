299aff6ed5d06630563c653ba1e7b5abffba6249
Daniel Rodriguez Aires <dannroda@gmail.com>
2022-11-13 07:23:57 -0300

    micro ui update, musica y funcion que registra posicion y tiempo en un array

--- Boton.gd
+++ Boton.gd
@@ -4,6 +4,9 @@ var toca_centro = false
 var toca_der = false
 var toca_externo = false
 var falla = false
+var elemento = "GGH"
+var posiciones = []
+onready var tema = get_parent().get_node("Cancion")
 func _ready():
 		$Nota.position.x = 500
 var teclas_cancion = load("res://Notas.tscn").instance()
@@ -11,11 +14,17 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_down"):
 		teclas_cancion.position = Vector2(500,0)
 		add_child(teclas_cancion)
-
+	if Input.is_action_just_pressed("ui_left"):
+		posiciones.append([teclas_cancion.position.x,tema.get_playback_position()])
+	if Input.is_action_just_pressed("ui_accept"):
+		print(posiciones)
+	if teclas_cancion.position.x == -30:
+		remove_child(teclas_cancion)
 	if teclas_cancion:
 		teclas_cancion.position.x -= 100 * delta
 	if $Nota.position.x > 0:
 		$Nota.position.x -= 100 * delta
+		
 	if Input.is_action_just_pressed("ui_up"):
 		if toca_externo == true:
 			if toca_izq && toca_centro && toca_der:
@@ -26,7 +35,6 @@ func _process(delta):
 			else:
 				falla = true
 				print("AFUEEERA")
-
 
 
 func _on_Nota_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
